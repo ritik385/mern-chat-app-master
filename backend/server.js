334a7647ac39321305975458696d075c274da68a
ritik385 <137667831+ritik385@users.noreply.github.com>
2024-04-07 12:03:05 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from "path"; // Import path module
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 6000;
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname); // Get current directory
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend", "dist"))); // Adjust path for static files
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
+});
